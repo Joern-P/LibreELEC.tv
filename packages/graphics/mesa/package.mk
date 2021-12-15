ce2d67ad9c147940e852a97cfe45cc511e2584bc
joern-p <ahap1@web.de>
2021-12-15 16:40:00 +0100

    update packages

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,12 +3,12 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="21.0.3"
-PKG_SHA256="565c6f4bd2d5747b919454fc1d439963024fc78ca56fd05158c3b2cde2f6912b"
+PKG_VERSION="21.3.1"
+PKG_SHA256="2b0dc2540cb192525741d00f706dbc4586349185dafc65729c7fda0800cc474d"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
-PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host"
+PKG_DEPENDS_TARGET="toolchain expat libdrm Mako:host zstd"
 PKG_LONGDESC="Mesa is a 3-D graphics library with an API."
 PKG_TOOLCHAIN="meson"
 
