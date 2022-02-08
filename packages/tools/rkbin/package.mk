4c4eb6cac8699fd4ed2b11c84e4b4ba6bbf76df6
joern-p <ahap1@web.de>
2022-02-08 08:59:45 +0100

    oc

--- packages/tools/rkbin/package.mk
+++ packages/tools/rkbin/package.mk
@@ -3,11 +3,11 @@
 
 PKG_NAME="rkbin"
 # Version is: Kwiboo/tag:libreelec-4563e24
-PKG_VERSION="4563e249a3f47e7fcd47a4c3769b6c05683b6e9d"
-PKG_SHA256="0b3479117700bce9afea2110c1f027b626c76d99045802218b35a53606547d60"
+PKG_VERSION="10f67f42c0ae9fa215b49ceabbc3d97194e6a389"
+PKG_SHA256=""
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="nonfree"
 PKG_SITE="https://github.com/rockchip-linux/rkbin"
-PKG_URL="https://github.com/rockchip-linux/rkbin/archive/${PKG_VERSION}.tar.gz"
+PKG_URL="https://github.com/Caesar-github/rkbin/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="rkbin: Rockchip Firmware and Tool Binaries"
 PKG_TOOLCHAIN="manual"
