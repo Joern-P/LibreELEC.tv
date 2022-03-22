82f962278074438871ea24fe8dc4ada0e909d79a
joern-p <ahap1@web.de>
2022-03-22 19:33:33 +0100

    custom rk3399 nanopc-t

--- packages/tools/u-boot/package.mk
+++ packages/tools/u-boot/package.mk
@@ -23,8 +23,8 @@ PKG_NEED_UNPACK="${PROJECT_DIR}/${PROJECT}/bootloader"
 
 case "${PROJECT}" in
   Rockchip)
-    PKG_VERSION="8659d08d2b589693d121c1298484e861b7dafc4f"
-    PKG_SHA256="3f9f2bbd0c28be6d7d6eb909823fee5728da023aca0ce37aef3c8f67d1179ec1"
+    PKG_VERSION="524f26463d9251cb8eef248c36eec33271f06dcb"
+    PKG_SHA256=""
     PKG_URL="https://github.com/rockchip-linux/u-boot/archive/${PKG_VERSION}.tar.gz"
     PKG_PATCH_DIRS="rockchip"
     ;;
