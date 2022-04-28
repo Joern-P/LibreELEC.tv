02b0c1b6a25ab30f09922cd4a26916d2c069fef7
joern-p <ahap1@web.de>
2022-04-28 08:54:56 +0200

    update sdl2

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="SDL2"
-PKG_VERSION="2.0.20"
-PKG_SHA256="c56aba1d7b5b0e7e999e4a7698c70b63a3394ff9704b5f6e1c57e0c16f04dd06"
+PKG_VERSION="2.0.22"
+PKG_SHA256="826e83c7a602b2025647e93c6585908379179f68d479dfc1d9b03d2b9570c8d9"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
 PKG_URL="https://www.libsdl.org/release/${PKG_NAME}-${PKG_VERSION}.tar.gz"
