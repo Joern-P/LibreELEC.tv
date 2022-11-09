2bcdfe3bf1addb2a9f9258ba2f20445c8b5babc3
joern-p <ahap1@web.de>
2022-11-09 10:16:57 +0100

    update packages

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="92a0283f169270dc205834b470c09c84e845dfc1"
-PKG_SHA256="51e2a51d98098662c138ffbcf8c08038e10f9b8b6b933b1a1180f3ec17950f96"
+PKG_VERSION="0850bb2c266aca68533faa8f551d8c9cfae7c951"
+PKG_SHA256="ce564f7b80e443261df44024afaca44f4f7c0df6ae87f3d290582d055a934de1"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
