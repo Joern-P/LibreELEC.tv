02bb61fb7c48a3511872f4a83cf4b63b4a686b3c
joern-p <ahap1@web.de>
2023-01-31 15:33:34 +0100

    cechack

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -10,8 +10,3 @@ PKG_DEPENDS_TARGET="toolchain ${PROJECT}"
 PKG_SECTION="virtual"
 PKG_LONGDESC="OEM: Metapackage for various OEM packages"
 
-# Entware support
-if [ "$ENTWARE_SUPPORT" = "yes" ]; then
-  ln -sf /storage/.opt $INSTALL/opt
-  PKG_DEPENDS_TARGET+=" entware"
-fi
