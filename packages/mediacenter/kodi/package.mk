97f0e19a21fc07d560aea256195bb1f14f66b9da
joern-p <ahap1@web.de>
2023-03-06 17:02:23 +0100

    update some packs

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="kodi"
-PKG_VERSION="cc00d8f5197483d33a27d4d827f17fbf99febddf"
-PKG_SHA256="d42751fd930f25bdeca251a9bdfa6bf8f52acb596a10bdeab7cf4def629d3572"
+PKG_VERSION="0e7b997584a6db5a0665d890b00707eeca6fd67b"
+PKG_SHA256="beb5beef1e86d5216a8ce093784b33f7b56cf688ce78f9f61d4585e4590a7a24"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.kodi.tv"
 PKG_URL="https://github.com/xbmc/xbmc/archive/${PKG_VERSION}.tar.gz"
--- packages/python/devel/meson/package.mk
+++ packages/python/devel/meson/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="meson"
-PKG_VERSION="1.0.0"
-PKG_SHA256="aa50a4ba4557c25e7d48446abfde857957dcdf58385fffbe670ba0e8efacce05"
+PKG_VERSION="1.0.1"
+PKG_SHA256="d926b730de6f518728cc7c57bc5e701667bae0c3522f9e369427b2cc7839d3c1"
 PKG_LICENSE="Apache"
 PKG_SITE="http://mesonbuild.com"
 PKG_URL="https://github.com/mesonbuild/meson/releases/download/${PKG_VERSION}/${PKG_NAME}-${PKG_VERSION}.tar.gz"
