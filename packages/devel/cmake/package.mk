43dc539e6f31c6cf54066c31448f8abc9a9f6bfb
joern-p <ahap1@web.de>
2023-09-15 10:28:25 +0200

    update kodi

--- packages/devel/cmake/package.mk
+++ packages/devel/cmake/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="cmake"
-PKG_VERSION="3.27.2"
-PKG_SHA256="798e50085d423816fe96c9ef8bee5e50002c9eca09fed13e300de8a91d35c211"
+PKG_VERSION="3.27.4"
+PKG_SHA256="0a905ca8635ca81aa152e123bdde7e54cbe764fdd9a70d62af44cad8b92967af"
 PKG_LICENSE="BSD"
 PKG_SITE="https://cmake.org/"
 PKG_URL="https://cmake.org/files/v$(get_pkg_version_maj_min)/cmake-${PKG_VERSION}.tar.gz"
