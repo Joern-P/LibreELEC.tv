730fd5a66ba28963c880d2755a94cc7c91cf0df1
joern-p <ahap1@web.de>
2023-09-29 14:28:42 +0200

    update pack

--- packages/devel/glib/package.mk
+++ packages/devel/glib/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="glib"
-PKG_VERSION="2.77.2"
-PKG_SHA256="16279739e4d30ec47be3e82909f5aeaaa41a8206bae3bead10a23fb2deff02a6"
+PKG_VERSION="2.78.0"
+PKG_SHA256="44eaab8b720877ce303c5540b657b126f12dc94972d9880b52959f43fb537b30"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://www.gtk.org/"
 PKG_URL="https://download.gnome.org/sources/glib/$(get_pkg_version_maj_min)/${PKG_NAME}-${PKG_VERSION}.tar.xz"
--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="23.2.0-rc2"
-PKG_SHA256=""
+PKG_VERSION="23.2.1"
+PKG_SHA256="64de0616fc2d801f929ab1ac2a4f16b3e2783c4309a724c8a259b20df8bbc1cc"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://mesa.freedesktop.org/archive/mesa-${PKG_VERSION}.tar.xz"
